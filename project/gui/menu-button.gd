7fd95a7a724cb4312c4f6e39193884a972b66315
Martin <martin@seventy2eleven.de>
2019-05-02 21:49:55 +0200

    Menu and art.

---
+++ project/gui/menu-button.gd
@@ -0,0 +1,28 @@
+extends AnimationPlayer
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(String) var text = "Label Text" setget set_text, get_text
+
+func set_text(text):
+	$"scale-node/label-node".text = text
+
+func get_text():
+	return $"scale-node/label-node".text 
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_labeltext_mouse_entered():
+	play("hover")
+
+
+func _on_labeltext_mouse_exited():
+	play("normal")
